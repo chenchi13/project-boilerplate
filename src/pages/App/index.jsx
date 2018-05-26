eb833d85822b1994f4e727ecedbbbc018142a1b3
Slaven Olujic <olujic.slaven@gmail.com>
2018-05-26 14:58:43 +0200

    test

--- src/pages/App/index.jsx
+++ src/pages/App/index.jsx
@@ -3,15 +3,17 @@ import { Route, Switch } from 'react-router-dom';
 import { Home, NotFound } from '../index';
 
 class App extends React.Component {
+
+   
   render() {
     return (
       <div>
         <Switch>
           {
-            // Define all your routes here
+            // Define all your routes here bla
           }
           <Route exact path="/" component={Home} />
-          <Route component={NotFound} />
+          <Route component={NotFound} /> 
         </Switch>
       </div>
     );
